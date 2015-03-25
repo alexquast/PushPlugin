a2ee15308275ddb2d6b91fa1eeeea82fc0a3a792
Alexander Quast <alexander.quast@open-xchange.com>
2015-03-25 09:54:12 +0100

    Added dummy relogin logic with hard coded test credentials

--- src/ios/AppDelegate+notification.m
+++ src/ios/AppDelegate+notification.m
@@ -10,13 +10,18 @@
 #import "PushPlugin.h"
 #import <objc/runtime.h>
 
+typedef void(^BackgroundCompletionHandler)(UIBackgroundFetchResult);
+
 static char launchNotificationKey;
+BackgroundCompletionHandler _backgroundCompletionHandler;
+NSURLSessionConfiguration *backgroundConfiguration;
+NSURLSession *session;
 
 @implementation AppDelegate (notification)
 
 - (id) getCommandInstance:(NSString*)className
 {
-	return [self.viewController getCommandInstance:className];
+    return [self.viewController getCommandInstance:className];
 }
 
 // its dangerous to override a method from within a category.
@@ -24,7 +29,7 @@ static char launchNotificationKey;
 + (void)load
 {
     Method original, swizzled;
-    
+
     original = class_getInstanceMethod(self, @selector(init));
     swizzled = class_getInstanceMethod(self, @selector(swizzled_init));
     method_exchangeImplementations(original, swizzled);
@@ -32,24 +37,24 @@ static char launchNotificationKey;
 
 - (AppDelegate *)swizzled_init
 {
-	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(createNotificationChecker:)
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(createNotificationChecker:)
                name:@"UIApplicationDidFinishLaunchingNotification" object:nil];
-	
-	// This actually calls the original init method over in AppDelegate. Equivilent to calling super
-	// on an overrided method, this is not recursive, although it appears that way. neat huh?
-	return [self swizzled_init];
+
+    // This actually calls the original init method over in AppDelegate. Equivilent to calling super
+    // on an overrided method, this is not recursive, although it appears that way. neat huh?
+    return [self swizzled_init];
 }
 
 // This code will be called immediately after application:didFinishLaunchingWithOptions:. We need
 // to process notifications in cold-start situations
 - (void)createNotificationChecker:(NSNotification *)notification
 {
-	if (notification)
-	{
-		NSDictionary *launchOptions = [notification userInfo];
-		if (launchOptions)
-			self.launchNotification = [launchOptions objectForKey: @"UIApplicationLaunchOptionsRemoteNotificationKey"];
-	}
+    if (notification)
+    {
+        NSDictionary *launchOptions = [notification userInfo];
+        if (launchOptions)
+            self.launchNotification = [launchOptions objectForKey: @"UIApplicationLaunchOptionsRemoteNotificationKey"];
+    }
 }
 
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
@@ -63,14 +68,14 @@ static char launchNotificationKey;
 }
 
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
-    NSLog(@"didReceiveNotification");
-    
+    NSLog(@"didReceiveNotification normal");
+
     // Get application state for iOS4.x+ devices, otherwise assume active
     UIApplicationState appState = UIApplicationStateActive;
     if ([application respondsToSelector:@selector(applicationState)]) {
         appState = application.applicationState;
     }
-    
+
     if (appState == UIApplicationStateActive) {
         PushPlugin *pushHandler = [self getCommandInstance:@"PushPlugin"];
         pushHandler.notificationMessage = userInfo;
@@ -82,16 +87,116 @@ static char launchNotificationKey;
     }
 }
 
+- (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didCompleteWithError:(NSError *)error {
+   NSLog(@"didcompletewith error %@",[error localizedDescription]);
+    _backgroundCompletionHandler(UIBackgroundFetchResultFailed);
+}
+
+-(void)URLSessionDidFinishEventsForBackgroundURLSession:(NSURLSession *)session {
+    NSLog(@"URLSessionDidFinishEventsForBackgroundURLSession");
+    _backgroundCompletionHandler(UIBackgroundFetchResultNewData);
+}
+
+
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult result))handler
+{
+    // store the completition handler to be called from the delegate
+    _backgroundCompletionHandler = handler;
+
+    NSLog(@"didReceiveNotification");
+    for(NSString *key in [userInfo allKeys]) {
+        NSLog(@"%@",[userInfo objectForKey:key]);
+    }
+    // server address, needs to be stored by wizard in main app
+    NSString *address = @"http://dovetest.oxoe.int/appsuite/api/login/httpAuth";
+    // create valid url and POST request and request body
+    NSURL *url = [NSURL URLWithString:address];
+
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    //[request setHTTPMethod:@"POST"];
+
+    // create basic auth params
+    NSString *authStr = @"user2@dovetest.oxoe.int:secret";
+    NSData *authData = [authStr dataUsingEncoding:NSUTF8StringEncoding];
+    NSString *authValue = [NSString stringWithFormat: @"Basic %@",[authData base64EncodedStringWithOptions:0]];
+    [request setValue:authValue forHTTPHeaderField:@"Authorization"];
+
+    // Get application state for iOS4.x+ devices, otherwise assume active
+
+    UIApplicationState appState = UIApplicationStateActive;
+
+    if ([application respondsToSelector:@selector(applicationState)]) {
+        appState = application.applicationState;
+    }
+    // app is active/forground, pass push to running app
+    if (appState == UIApplicationStateActive) {
+        PushPlugin *pushHandler = [self getCommandInstance:@"PushPlugin"];
+        pushHandler.notificationMessage = userInfo;
+        pushHandler.isInline = YES;
+        [pushHandler notificationReceived];
+
+        // call when work is done
+        handler(UIBackgroundFetchResultNewData);
+
+    } else if(appState == UIApplicationStateBackground) {
+        if ([userInfo valueForKeyPath:@"aps.content-available"]) {
+
+            NSLog(@"Background relogin needed");
+
+            NSURLSessionDownloadTask *downloadTask = [session downloadTaskWithRequest:request];
+            //NSURLSessionUploadTask *uTask = [session uploadTaskWithRequest:request fromFile:fileURL];
+
+            NSLog(@"Starting relogin via basic auth");
+
+            [downloadTask resume];
+        }
+    } else {
+        NSLog(@"Background but not relogin");
+        //save it for later
+        self.launchNotification = userInfo;
+        // call when work is done
+        handler(UIBackgroundFetchResultNewData);
+
+    }
+
+}
+/*
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    NSLog(@"didReceiveNotification");
+
+    // Get application state for iOS4.x+ devices, otherwise assume active
+    UIApplicationState appState = UIApplicationStateActive;
+    if ([application respondsToSelector:@selector(applicationState)]) {
+        appState = application.applicationState;
+    }
+
+    if (appState == UIApplicationStateActive) {
+        PushPlugin *pushHandler = [self getCommandInstance:@"PushPlugin"];
+        pushHandler.notificationMessage = userInfo;
+        pushHandler.isInline = YES;
+        [pushHandler notificationReceived];
+    } else {
+        //save it for later
+        self.launchNotification = userInfo;
+    }
+}
+*/
+
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    
+
     NSLog(@"active");
-    
+
+    backgroundConfiguration = [NSURLSessionConfiguration backgroundSessionConfigurationWithIdentifier:@"com.open-xchange.mailapp"];
+    session = [NSURLSession sessionWithConfiguration:backgroundConfiguration delegate:self delegateQueue:[NSOperationQueue mainQueue]];
+
+
     //zero badge
     application.applicationIconBadgeNumber = 0;
 
     if (self.launchNotification) {
         PushPlugin *pushHandler = [self getCommandInstance:@"PushPlugin"];
-		
+
         pushHandler.notificationMessage = self.launchNotification;
         self.launchNotification = nil;
         [pushHandler performSelectorOnMainThread:@selector(notificationReceived) withObject:pushHandler waitUntilDone:NO];
@@ -112,7 +217,7 @@ static char launchNotificationKey;
 
 - (void)dealloc
 {
-    self.launchNotification	= nil; // clear the association and release the object
+    self.launchNotification = nil; // clear the association and release the object
 }
 
 @end
