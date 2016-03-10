6797537af3e3b7c8c78e1fea567aa8e927635d8e
Jan Lahni <jan.lahni@open-xchange.com>
2016-03-10 17:51:43 +0100

    Extend notification plugin to read color code from shared preferences and set the notification light color to this color

--- src/android/com/plugin/gcm/GCMIntentService.java
+++ src/android/com/plugin/gcm/GCMIntentService.java
@@ -13,6 +13,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
@@ -22,204 +23,254 @@ import com.openexchange.mobile.mailapp.enterprise.R;
 
 @SuppressLint("NewApi")
 public class GCMIntentService extends GCMBaseIntentService {
-
-	private static final String TAG = "GCMIntentService";
-
-	public GCMIntentService() {
-		super("GCMIntentService");
-	}
-
-	@Override
-	public void onRegistered(Context context, String regId) {
-
-		Log.v(TAG, "onRegistered: "+ regId);
-
-		//BEGIN initialize custom notification handling
-
-		//need a new random notification ID, to update the current notification
-		Random rand = new Random();
-		int notificationId = rand.nextInt();
-		SharedPreferences preferences = context.getSharedPreferences("notifications", Context.MODE_PRIVATE);
-		SharedPreferences.Editor store = preferences.edit();
-		store
-			.clear()
-			.putInt("id", notificationId)
-			.putInt("messageCount", 0);
-		store.apply();
-
-		//DONE initialize custom notification handling
-
-		JSONObject json;
-
-		try
-		{
-			json = new JSONObject().put("event", "registered");
-			json.put("regid", regId);
-
-			Log.v(TAG, "onRegistered: " + json.toString());
-
-			// Send this JSON data to the JavaScript application above EVENT should be set to the msg type
-			// In this case this is the registration ID
-			PushPlugin.sendJavascript( json );
-
-		}
-		catch( JSONException e)
-		{
-			// No message to the user is sent, JSON failed
-			Log.e(TAG, "onRegistered: JSON exception");
-		}
-	}
-
-	@Override
-	public void onUnregistered(Context context, String regId) {
-		Log.d(TAG, "onUnregistered - regId: " + regId);
-	}
-
-	@Override
-	protected void onMessage(Context context, Intent intent) {
-		Log.d(TAG, "onMessage - context: " + context);
-
-		// Extract the payload from the message
-		Bundle extras = intent.getExtras();
-		if (extras != null)
-		{
-			Log.d(TAG, "got extras from push");
-			// if we are in the foreground, just surface the payload, else post it to the statusbar
+    
+    private static final String TAG = "GCMIntentService";
+    
+    public GCMIntentService() {
+        super("GCMIntentService");
+    }
+    
+    @Override
+    public void onRegistered(Context context, String regId) {
+        
+        Log.v(TAG, "onRegistered: " + regId);
+        
+        //BEGIN initialize custom notification handling
+        
+        //need a new random notification ID, to update the current notification
+        Random rand = new Random();
+        int notificationId = rand.nextInt();
+        SharedPreferences preferences = context.getSharedPreferences("notifications", Context.MODE_PRIVATE);
+        SharedPreferences.Editor store = preferences.edit();
+        store
+        .clear()
+        .putInt("id", notificationId)
+        .putInt("messageCount", 0);
+        store.apply();
+        
+        //DONE initialize custom notification handling
+        
+        JSONObject json;
+        
+        try
+        {
+            json = new JSONObject().put("event", "registered");
+            json.put("regid", regId);
+            
+            Log.v(TAG, "onRegistered: " + json.toString());
+            
+            // Send this JSON data to the JavaScript application above EVENT should be set to the msg type
+            // In this case this is the registration ID
+            PushPlugin.sendJavascript( json );
+            
+        }
+        catch( JSONException e)
+        {
+            // No message to the user is sent, JSON failed
+            Log.e(TAG, "onRegistered: JSON exception");
+        }
+    }
+    
+    @Override
+    public void onUnregistered(Context context, String regId) {
+        Log.d(TAG, "onUnregistered - regId: " + regId);
+    }
+    
+    @Override
+    protected void onMessage(Context context, Intent intent) {
+        Log.d(TAG, "onMessage - context: " + context);
+        
+        // Extract the payload from the message
+        Bundle extras = intent.getExtras();
+        if (extras != null)
+        {
+            Log.d(TAG, "got extras from push");
+            // if we are in the foreground, just surface the payload, else post it to the statusbar
             if (PushPlugin.isInForeground()) {
-				extras.putBoolean("foreground", true);
+                extras.putBoolean("foreground", true);
                 PushPlugin.sendExtras(extras);
-			}
-			else {
-				extras.putBoolean("foreground", false);
+            }
+            else {
+                extras.putBoolean("foreground", false);
                	// standard case, a new mail arrives. Build notification and show it
                	createNotification(context, extras);
             }
         }
-	}
-
-
-
-	public void createNotification(Context context, Bundle extras) {
-		SharedPreferences preferences = context.getSharedPreferences("notifications", Context.MODE_PRIVATE);
-		NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-		String appName = getAppName(this);
-		Intent notificationIntent = new Intent(this, PushHandlerActivity.class);
-		notificationIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
-		int messageCount = preferences.getInt("messageCount", 0);
-		if (messageCount > 0) {
-			//remove cid from extras, so App opens in default folder
-			extras.remove("cid");
-		}
-		notificationIntent.putExtra("pushBundle", extras);
-
-		PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-		int defaults = Notification.DEFAULT_ALL;
-
-		if (extras.getString("defaults") != null) {
-			try {
-				defaults = Integer.parseInt(extras.getString("defaults"));
-			} catch (NumberFormatException e) {}
-		}
-
-		Log.d(TAG, "Got a message");
-
-		String message = extras.getString("message");
-
-		// handle different versions of push messages here
-		String subject = extras.getString("subject");
-		String sender = extras.getString("sender");
-		if (sender == null && subject == null) {
-
-			// check if refresh event
-			String sync_event = extras.getString("SYNC_EVENT");
-
-			if (sync_event != null && sync_event.equals("MAIL")) {
-				Log.d(TAG, "Got refresh event for mail in background, doing nothing");
-				return;
-			}
-
-			Boolean isRefresh = message.equals("You've received a new login");
-			if (isRefresh) {
-				Log.d(TAG, "Got a relogin message, this should not happen as it is deprecated.");
-				return;
-			}
-
-			// use old style message format, just for backwards compatibility
-			String subjectAndSender[] = message.split("\\n");
-			subject = subjectAndSender[1];
-			sender = subjectAndSender[0];
-		}
-
-
-		// small Icon is the small one placed on bottom right on the large one
-		// Large Icon could be the contact image, small icon the app icon
-		// ATM the large icon is the app icon, small icon is not needed
-		NotificationCompat.Builder mBuilder =
-			new NotificationCompat.Builder(context)
-				.setDefaults(defaults)
-				.setSmallIcon(R.drawable.ic_action_email)
-				.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.icon))
-				//.setSmallIcon(context.getApplicationInfo().icon)
-				.setWhen(System.currentTimeMillis())
-				.setTicker(sender)
-                .setLights(0xFF0000FF, 1000, 2000) // set blue notification light which pulses
-				.setContentIntent(contentIntent)
-				.setAutoCancel(true);
-
-		if (subject == null) {
-			subject = "";
-			Log.d(TAG, "Missing subject or sender for message");
-		}
-
-		messageCount++;
-		SharedPreferences.Editor store = preferences.edit();
-		store.putInt("messageCount", messageCount);
-		store.apply();
-
-		if (messageCount > 1) {
-			mBuilder.setContentTitle(getResources().getText(R.string.new_messages));
-			String content = getResources().getQuantityString(R.plurals.got_number_new_messages, messageCount, messageCount);
-			mBuilder.setContentText(content);
-			Log.d(TAG, "Switch to unspecific notification: " + content);
-		} else {
-			mBuilder
-				.setContentTitle(sender)
-				.setContentText(subject);
-		}
-
+    }
+    
+    
+    
+    public void createNotification(Context context, Bundle extras) {
+        SharedPreferences preferences = context.getSharedPreferences("notifications", Context.MODE_PRIVATE);
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        String appName = getAppName(this);
+        Intent notificationIntent = new Intent(this, PushHandlerActivity.class);
+        notificationIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        
+        int messageCount = preferences.getInt("messageCount", 0);
+        if (messageCount > 0) {
+            //remove cid from extras, so App opens in default folder
+            extras.remove("cid");
+        }
+        notificationIntent.putExtra("pushBundle", extras);
+        
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        
+        int defaults = Notification.DEFAULT_ALL;
+        
+        if (extras.getString("defaults") != null) {
+            try {
+                defaults = Integer.parseInt(extras.getString("defaults"));
+            } catch (NumberFormatException e) {}
+        }
+        
+        Log.d(TAG, "Got a message");
+        
+        String message = extras.getString("message");
+        
+        // handle different versions of push messages here
+        String subject = extras.getString("subject");
+        String sender = extras.getString("sender");
+        if (sender == null && subject == null) {
+            
+            // check if refresh event
+            String sync_event = extras.getString("SYNC_EVENT");
+            
+            if (sync_event != null && sync_event.equals("MAIL")) {
+                Log.d(TAG, "Got refresh event for mail in background, doing nothing");
+                return;
+            }
+            
+            Boolean isRefresh = message.equals("You've received a new login");
+            if (isRefresh) {
+                Log.d(TAG, "Got a relogin message, this should not happen as it is deprecated.");
+                return;
+            }
+            
+            // use old style message format, just for backwards compatibility
+            String subjectAndSender[] = message.split("\\n");
+            subject = subjectAndSender[1];
+            sender = subjectAndSender[0];
+        }
+        
+        
+        // small Icon is the small one placed on bottom right on the large one
+        // Large Icon could be the contact image, small icon the app icon
+        // ATM the large icon is the app icon, small icon is not needed
+        
+        NotificationCompat.Builder mBuilder =
+        new NotificationCompat.Builder(context)
+        .setDefaults(defaults)
+        .setSmallIcon(R.drawable.ic_action_email)
+        .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.icon))
+        //.setSmallIcon(context.getApplicationInfo().icon)
+        .setWhen(System.currentTimeMillis())
+        .setTicker(sender)
+        //.setLights(0xFF0000FF, 1000, 2000) // set blue notification light which pulses
+        .setContentIntent(contentIntent)
+        .setAutoCancel(true);
+        
+        Integer color = getNotificationColour(context);
+        if (color == null){
+            mBuilder.setLights(0xFF0000FF, 0, 2000);
+        } else {
+            Log.v(TAG, "setLights to color: " + color);
+            mBuilder.setLights(color, 1000, 2000);
+        }
+        
+        if (subject == null) {
+            subject = "";
+            Log.d(TAG, "Missing subject or sender for message");
+        }
+        
+        messageCount++;
+        SharedPreferences.Editor store = preferences.edit();
+        store.putInt("messageCount", messageCount);
+        store.apply();
+        
+        if (messageCount > 1) {
+            mBuilder.setContentTitle(getResources().getText(R.string.new_messages));
+            String content = getResources().getQuantityString(R.plurals.got_number_new_messages, messageCount, messageCount);
+            mBuilder.setContentText(content);
+            Log.d(TAG, "Switch to unspecific notification: " + content);
+        } else {
+            mBuilder
+            .setContentTitle(sender)
+            .setContentText(subject);
+        }
+        
         Notification notification = mBuilder.build();
         notification.defaults = 0;
         notification.flags |= Notification.FLAG_SHOW_LIGHTS;
         notification.defaults = Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE;
-		int notificationId = preferences.getInt("id", 0);
-		if (notificationId != 0) {
-			mNotificationManager.notify(notificationId, notification);
-		} else {
-			Log.wtf(TAG, "No notification ID found in SharedPreferences");
-		}
-	}
-
-	private static String getAppName(Context context)
-	{
-		CharSequence appName =
-				context
-					.getPackageManager()
-					.getApplicationLabel(context.getApplicationInfo());
-
-		return (String)appName;
-	}
-
-	@Override
-	public void onError(Context context, String errorId) {
-		Log.e(TAG, "onError - errorId: " + errorId);
-	}
-
-	public static void resetNewMessageCounter(Context context) {
-		SharedPreferences preferences = context.getSharedPreferences("notifications", Context.MODE_PRIVATE);
-		SharedPreferences.Editor store = preferences.edit();
-		store.putInt("messageCount", 0);
-		store.apply();
-	}
+        int notificationId = preferences.getInt("id", 0);
+        if (notificationId != 0) {
+            mNotificationManager.notify(notificationId, notification);
+        } else {
+            Log.wtf(TAG, "No notification ID found in SharedPreferences");
+        }
+    }
+    
+    
+    
+    private static String getAppName(Context context)
+    {
+        CharSequence appName =
+        context
+        .getPackageManager()
+        .getApplicationLabel(context.getApplicationInfo());
+        
+        return (String)appName;
+    }
+    
+    
+    /*
+     * Load the notification light color which is stored in the shared preferences under key "notificationColor" amd check if it is prober RGB color in RGB
+     */
+    
+    private Integer getNotificationColour (Context context){
+        SharedPreferences sharedPreferences = context.getSharedPreferences("Preferences", Context.MODE_PRIVATE);
+        String value = sharedPreferences.getString("notificationColor", null);
+        Log.v(TAG, "loaded: " + value);
+        Integer color = null;
+        Log.v(TAG, "Color: " + color);
+        if(value != null){
+            try {
+                //color = Integer.parseInt(value, 16);
+                color = Color.parseColor(value);
+                Log.v(TAG, "ParsedColor: " + color);
+            } catch (IllegalArgumentException e) {
+                color = null;
+                Log.v(TAG, "ongetNotificationColor: IAException " + e.getMessage());
+            }
+        }
+        Log.v(TAG, "return: " + color);
+        return color;
+        
+        
+        
+        /*String test = "123456789ABCDEF";
+         if (value != null && value.length() == 6){
+         for (int i= 0; i < 6; i++){
+         if(!test.contains(String.valueOf(value.charAt(i)))){
+         return null;
+         }
+         }
+         return value;
+         }
+         return null;*/
+    }
+    
+    @Override
+    public void onError(Context context, String errorId) {
+        Log.e(TAG, "onError - errorId: " + errorId);
+    }
+    
+    public static void resetNewMessageCounter(Context context) {
+        SharedPreferences preferences = context.getSharedPreferences("notifications", Context.MODE_PRIVATE);
+        SharedPreferences.Editor store = preferences.edit();
+        store.putInt("messageCount", 0);
+        store.apply();
+    }
 }
+
